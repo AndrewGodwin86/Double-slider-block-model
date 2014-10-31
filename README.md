b39f9ed5a234876382169ee886020f5bda7d939d
Andrew Godwin <andrew.godwin86@gmail.com>
2014-10-31 15:41:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Double-slider-block-model
+=========================
+
+Program simulating the double slider block model, a proposed earthquake modelling technique.
+
+This program is a JavaScript port of the original FORTRAN 95 program I wrote for my dissertation at Newcastle University in 2009.
---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Push initial values to input fields
+$('document').ready(function(){
+	var alphaInput = document.getElementById('alpha');
+	alphaInput.value = alpha;
+
+	var betaInput = document.getElementById('beta');
+	betaInput.value = beta;
+
+	var phiInput = document.getElementById('phi');
+	phiInput.value = phi;
+
+	var numSlipEventsInput = document.getElementById('numberOfSlipEvents');
+	numSlipEventsInput.value = nn;
+
+	var y1InitVal = document.getElementById('y1Init');
+	y1InitVal.value = y1;
+
+	var y2InitVal = document.getElementById('y2Init');
+	y2InitVal.value = y2;	
+
+});
+
